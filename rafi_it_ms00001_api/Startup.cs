17d081896674f526145fa7bf393509e2fa6b2e83
cocoytech <i.am.cocoytech+github@gmail.com>
2019-05-23 22:57:48 +0800

    Add Connection settings from model utility to startup

--- rafi_it_ms00001_api/Startup.cs
+++ rafi_it_ms00001_api/Startup.cs
@@ -15,12 +15,19 @@ namespace rafi_it_ms00001_api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
