1f3caf38896ad3693f1a8b25b8a6c496f0994770
cocoytech <i.am.cocoytech+github@gmail.com>
2019-05-27 15:17:15 +0800

    Add ActionFilter to centralize and declutter the model

---
+++ rafi_it_ms00001_api/Helpers/ValidationFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace rafi_it_ms00001_api.Helpers
+{
+    public class ValidationFilterAttribute : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState); // returns 400 with error
+            }
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
